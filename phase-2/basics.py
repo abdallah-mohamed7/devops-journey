c9af7ed07a399001c5db34fb1c81f12479228c59
Abdallah Mohamed <amahaaq@gmail.com>
2026-03-10 22:13:54 +0200

    phase-2: python basics, f-strings, loops, conditionals

---
+++ phase-2/basics.py
@@ -0,0 +1,17 @@
+
+name = "devops"
+version = 3.11
+
+print(f"Python {version} for {name}")
+
+servers = ["ai1", "web2", "database"]
+for server in servers:
+    print(f"Checking {server}...")
+for server in servers:
+      if server == "database":      
+              print(f"we find it {server}")
+      else:
+             print(f"data base not find ")
+if "database" in server :
+ print(f"we find it")
+
