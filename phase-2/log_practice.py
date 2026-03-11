d8aaff113c91c31af2095d2d339b5be6fa1d8c2a
Abdallah Mohamed <amahaaq@gmail.com>
2026-03-12 00:54:49 +0200

    phase-2: logging module, basicConfig, file output

---
+++ phase-2/log_practice.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log')
+
+
+logging.debug("DEBUG")
+logging.info("INFO")
+logging.warning("WARNING")
+logging.error("ERROR")
+logging.critical("CRITICAL")
