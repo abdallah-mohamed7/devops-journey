65ea7d94d3683835ab031dc1dfc5f249b5928b95
Abdallah Mohamed <amahaaq@gmail.com>
2026-04-09 17:42:27 +0200

    refactor: move to stateless architecture (JSON source)

--- hadith-api-code/src/main/java/com/example/demo/HadithController.java
+++ hadith-api-code/src/main/java/com/example/demo/HadithController.java
@@ -4,21 +4,21 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*") // This allows your HTML file to talk to this Java app
+@CrossOrigin(origins = "*")
 public class HadithController {
 
     private final HadithService hadithService;
 
-    // Spring automatically "injects" the service here
     public HadithController(HadithService hadithService) {
         this.hadithService = hadithService;
     }
 
     @GetMapping("/hadith")
-    public String getDailyHadith() {
-        return hadithService.getRamadanHadith();
+    public Map<String, Object> getDailyHadith() {
+        return hadithService.getDailyHadith();
     }
 }
