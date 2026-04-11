c1bbb0fa003c511eb348eb6c0f74affc3fcc783d
Abdallah Mohamed <amahaaq@gmail.com>
2026-04-11 11:28:20 +0200

    feat: consume APP_MESSAGE from enviroment

--- hadith-api-code/src/main/java/com/example/demo/HadithController.java
+++ hadith-api-code/src/main/java/com/example/demo/HadithController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +14,17 @@ public class HadithController {
 
     private final HadithService hadithService;
 
+    @Value("${APP_MESSAGE:Default Message}")
+    private String appMessage;
+
     public HadithController(HadithService hadithService) {
         this.hadithService = hadithService;
     }
 
     @GetMapping("/hadith")
     public Map<String, Object> getDailyHadith() {
-        return hadithService.getDailyHadith();
+        Map<String, Object> response = hadithService.getDailyHadith();
+        response.put("message", appMessage);
+        return response;
     }
 }
